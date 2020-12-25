a3e93abf4e268ffcfafc102156d38b4d1186eee7
heluile <marceauhelory@gmail.com>
2020-12-25 14:36:22 +0100

    ajout constructor + get + set dans Silo.js

--- Projet/src/js/Silo.js
+++ Projet/src/js/Silo.js
@@ -1,6 +1,57 @@
 class Silo {
 
-  constructor(Cellule c1, Cellule c2){
+  constructor(tabCellule, tabSonde, gaineVentil, ventilateur){
+    this.hauteur=0;
+    this.volume=0;
+    this.tauxRempli=0;
+    this.temp=0;
+    this.tabCellule=tabCellule;
+    this.tabSonde=tabSonde;
+    this.gaineVentil=gaineVentil;
+    this.ventilateur=ventilateur;
+  }
 
+  getHauteur(){
+    return this.hauteur;
+  }
+
+  getVolume(){
+    return this.volume;
+  }
+
+  getTauxRempli(){
+    return this.tauxRempli;
+  }
+
+  getTemperature(){
+    return this.temp;
+  }
+
+  getTabCellule(){
+    for(var i=0; i<tabCellule.length; i++){
+      return tabCellule[i];
+    }
+  }
+
+  getTabSonde(){
+    for(var i=0; i<tabSonde.length; i++){
+      return tabSonde[i];
+    }
+  }
+
+  getCellule(i){
+    return this.tabCellule[i];
+  }
+
+  getSonde(i){
+    return this.tabSonde[i];
+  }
+
+  getGainVentil(){
+    return this.gaineVentil;
+  }
+
+  getVentil(){
+    return this.ventilateur;
   }
 }
