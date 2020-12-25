ed1f79f1c06659c55600813d875e4fa30afd80bf
heluile <marceauhelory@gmail.com>
2020-12-25 14:18:52 +0100

    ajout Cellule.js + constructor + get + set + alarme(bool)

---
+++ Projet/src/js/Cellule.js
@@ -0,0 +1,37 @@
+class Cellule {
+
+  constructor(id, temperature, remplissage){
+    this.id=id;
+    this.temperature=temperature;
+    this.remplissage=remplissage;
+    this.alarme=new Boolean(false);
+  }
+
+  getId(){
+    return this.id;
+  }
+
+  getTemperature(){
+    return this.temperature;
+  }
+
+  getRemplissage(){
+    return this.remplissage;
+  }
+
+  setId(id){
+    this.id=id;
+  }
+
+  setTemperature(temperature){
+    this.temperature=temperature;
+  }
+
+  setRemplissage(remplissage){
+    this.remplissage=remplissage;
+  }
+
+  alarme(){
+    this.alarme=true;
+  }
+}
