6712d3cb21d868047a2c6a011374d0375c65d9a2
heluile <marceauhelory@gmail.com>
2021-01-04 16:35:26 +0100

    gfgjdjd

--- Projet/src/app/app-routing.module.ts
+++ Projet/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SiloComponent} from './silo/silo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path : "silo", component : SiloComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
